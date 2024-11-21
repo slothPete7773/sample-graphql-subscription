8e5ec9a5dd32d66c7b719818edd0d32eca14c118
Veerakit SlothPete Prasertpol <veerakit413@gmail.com>
2024-11-21 21:50:57 +0700

    refactor: refactor package

--- src/core/modules/resolver.go
+++ src/core/modules/resolver.go
@@ -2,10 +2,6 @@ package core
 
 import (
 	"sample-subscription/src/core/modules/message"
-	"strings"
-	"time"
-
-	"github.com/google/uuid"
 )
 
 type Resolver struct {
@@ -27,42 +23,4 @@ func NewResolver() *Resolver {
 	go r.BroadcastMessageEvent()
 
 	return &r
-}
-
-func (r *Resolver) BroadcastMessageEvent() {
-	subscribers := map[string]*message.OnMessageSubscriber{}
-	unsubscribe := make(chan string)
-
-	// NOTE: subscribing and sending events are at odds.
-	for {
-		select {
-		case id := <-unsubscribe:
-			delete(subscribers, id)
-		case s := <-r.HelloSaidSubscriber:
-			subscribers[uuid.NewString()] = s
-		case e := <-r.MessageEvents:
-			for id, s := range subscribers {
-				go func(id string, s *message.OnMessageSubscriber) {
-					select {
-					case <-s.Stop:
-						unsubscribe <- id
-						return
-					default:
-					}
-
-					if s.Filter != "" && !strings.Contains(e.Msg, s.Filter) {
-						// Event does not match filter, skip sending
-						return
-					}
-
-					select {
-					case <-s.Stop:
-						unsubscribe <- id
-					case s.Events <- e:
-					case <-time.After(time.Second):
-					}
-				}(id, s)
-			}
-		}
-	}
 }
--- src/main.go
+++ src/main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 	"os"
+	core "sample-subscription/src/core/modules"
 	"sample-subscription/src/subscription/graphqlws"
 	"strconv"
-	"strings"
-	"time"
 
-	"github.com/google/uuid"
 	graphql "github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/relay"
 )
@@ -35,7 +32,8 @@ func main() {
 	}
 
 	// init graphQL schema
-	s, err := graphql.ParseSchema(string(schema), newResolver(), graphql.UseFieldResolvers())
+	resolver := core.NewResolver()
+	s, err := graphql.ParseSchema(string(schema), resolver, graphql.UseFieldResolvers())
 	if err != nil {
 		panic(err)
 	}
@@ -48,96 +46,4 @@ func main() {
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", httpPort), nil); err != nil {
 		panic(err)
 	}
-}
-
-type message struct {
-	Id  string
-	Msg string
-}
-
-type resolver struct {
-	messageEvents       chan *message
-	helloSaidSubscriber chan *onMessageSubscriber
-}
-
-func newResolver() *resolver {
-	r := &resolver{
-		messageEvents:       make(chan *message),
-		helloSaidSubscriber: make(chan *onMessageSubscriber),
-	}
-
-	go r.broadcastMessageEvent()
-
-	return r
-}
-
-func (r *resolver) Hello() string {
-	return "Hello world!"
-}
-
-func (r *resolver) SendMessage(args struct{ Msg string }) *message {
-	e := &message{Msg: args.Msg, Id: uuid.New().String()}
-	go func() {
-		select {
-		case r.messageEvents <- e:
-		case <-time.After(1 * time.Second):
-		}
-	}()
-	return e
-}
-
-type onMessageSubscriber struct {
-	stop   <-chan struct{}
-	events chan<- *message
-	filter string
-}
-
-func (r *resolver) broadcastMessageEvent() {
-	subscribers := map[string]*onMessageSubscriber{}
-	unsubscribe := make(chan string)
-
-	// NOTE: subscribing and sending events are at odds.
-	for {
-		select {
-		case id := <-unsubscribe:
-			delete(subscribers, id)
-		case s := <-r.helloSaidSubscriber:
-			subscribers[uuid.NewString()] = s
-		case e := <-r.messageEvents:
-			for id, s := range subscribers {
-				go func(id string, s *onMessageSubscriber) {
-					select {
-					case <-s.stop:
-						unsubscribe <- id
-						return
-					default:
-					}
-
-					if s.filter != "" && !strings.Contains(e.Msg, s.filter) {
-						// Event does not match filter, skip sending
-						return
-					}
-
-					select {
-					case <-s.stop:
-						unsubscribe <- id
-					case s.events <- e:
-					case <-time.After(time.Second):
-					}
-				}(id, s)
-			}
-		}
-	}
-}
-
-func (r *resolver) OnMessage(ctx context.Context, args struct{ Filter *string }) <-chan *message {
-	c := make(chan *message)
-	filter := ""
-	if args.Filter != nil {
-		filter = *args.Filter
-	}
-	// NOTE: this could take a while
-	r.helloSaidSubscriber <- &onMessageSubscriber{events: c, stop: ctx.Done(), filter: filter}
-
-	return c
 }
